d482e63819cc2c8ee4fde05cec8bcf4450972a72
PSD\wilaiwan.a <wilaiwan.a@psd.co.th>
2026-01-21 13:26:09 +0700

    add btn export

--- src/app/transaction-log/transaction-log.ts
+++ src/app/transaction-log/transaction-log.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-transaction-log',
-  imports: [ TableModule, CommonModule ],
+  imports: [ TableModule, CommonModule, ButtonModule ],
   templateUrl: './transaction-log.html',
   styleUrl: './transaction-log.scss',
 })
